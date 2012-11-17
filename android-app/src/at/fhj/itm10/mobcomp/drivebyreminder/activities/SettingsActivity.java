c34040c6358acf302bfd85c058aa1dfa8f58bc0f
nohum <eiskalterengel@truthfactory.tk>
2012-11-17 21:41:31 +0000

    finished preferences

--- android-app/src/at/fhj/itm10/mobcomp/drivebyreminder/activities/SettingsActivity.java
+++ android-app/src/at/fhj/itm10/mobcomp/drivebyreminder/activities/SettingsActivity.java
@@ -2,12 +2,19 @@ package at.fhj.itm10.mobcomp.drivebyreminder.activities;
 
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import at.fhj.itm10.mobcomp.drivebyreminder.R;
 
 import com.actionbarsherlock.view.MenuItem;
 import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockPreferenceActivity;
 
+/**
+ * Settings activity.
+ * 
+ * @author Wolfgang Gaar
+ */
 public class SettingsActivity extends RoboSherlockPreferenceActivity {
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -15,6 +22,10 @@ public class SettingsActivity extends RoboSherlockPreferenceActivity {
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
 
+        
+        // Deprecated, but THE way to go to support pre
+        // android-3.0 devices
+        addPreferencesFromResource(R.xml.preferences);
 	}
 	
     @Override
