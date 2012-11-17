b416041259f5d27448f85b7175530571fb8b9c49
nohum <eiskalterengel@truthfactory.tk>
2012-11-17 14:40:45 +0000

    removed ldpi drawable directory

--- android-app/src/at/fhj/itm10/mobcomp/drivebyreminder/activities/SettingsActivity.java
+++ android-app/src/at/fhj/itm10/mobcomp/drivebyreminder/activities/SettingsActivity.java
@@ -1,7 +1,17 @@
 package at.fhj.itm10.mobcomp.drivebyreminder.activities;
 
+import android.os.Bundle;
+
 import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockPreferenceActivity;
 
 public class SettingsActivity extends RoboSherlockPreferenceActivity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+	}
+	
 }
