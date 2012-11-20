611ec751efcfba7be0aa18d38975e5b692171e7c
nohum <eiskalterengel@truthfactory.tk>
2012-11-20 21:13:56 +0000

    access restriction for front controller

--- server-app/web/index_dev.php
+++ server-app/web/index_dev.php
@@ -1,5 +1,15 @@
 <?php
 
+// Only allow local access to dev front controller
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+	|| isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+	|| !in_array(@$_SERVER['REMOTE_ADDR'], array(
+	        '127.0.0.1',
+	        '::1',
+))) {
+	header('HTTP/1.0 403 Forbidden');
+}
+
 ini_set('display_errors', 1);
 error_reporting(-1);
 
